f3b5831e6501e475801f680abc6dfb905afb6d60
wyonyango <wycliffealphus7@gmail.com>
2025-01-06 11:23:44 +0300

    feat:update handlers

--- main.go
+++ main.go
@@ -3,14 +3,18 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"urlShortener/handlers"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("Welcome to URL shortener!!")
+		fmt.Fprintln(w, "Welcome to URL shortener!!")
 	})
 
-	fmt.Println("Server started at:http://localhost:8080")
+	http.HandleFunc("/shorten", handlers.HandleShorten)
+	http.HandleFunc("/redirect", handlers.HandleRedirect)
 
+	fmt.Println("Server started at:http://localhost:8080")
 	http.ListenAndServe(":8080", nil)
 }
