b5f9a36d6b262a0274a90fee7398773d4d342fb4
wyonyango <wycliffealphus7@gmail.com>
2025-02-06 11:59:02 +0300

    feat: initialize  db in main

--- main.go
+++ main.go
@@ -4,11 +4,16 @@ import (
 	"fmt"
 	"net/http"
 
+	"urlShortener/db"
 	"urlShortener/routes"
 )
 
 func main() {
 	router := routes.InitRoutes()
+	dbPath := "db/database.sqlite3"
+	sqlFile := "db/schema.sql"
+
+	db.InitDB(dbPath, sqlFile)
 
 	fmt.Println("Server started at:http://localhost:8080")
 	http.ListenAndServe(":8080", router)
