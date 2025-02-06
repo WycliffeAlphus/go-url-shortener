ab604e1ef1d543da054196c77eb21b5a0ad3a42e
wyonyango <wycliffealphus7@gmail.com>
2025-02-06 11:46:38 +0300

    feat:initialize the database

---
+++ db/init_db.go
@@ -0,0 +1,31 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+)
+
+// InitDB initializes the SQLite database by executing the schema.sql script.
+func InitDB(dbPath, sqlFile string) {
+	db, err := sql.Open("sqlite3", dbPath)
+	if err != nil {
+		log.Fatalf("Error opening database: %v", err)
+	}
+
+	defer db.Close()
+
+	sqlBytes, err := os.ReadFile(sqlFile)
+	if err != nil {
+		log.Fatalf("Error reading SQL file: %v", err)
+	}
+	sqlStatements := string(sqlBytes)
+
+	_, err = db.Exec(sqlStatements)
+	if err != nil {
+		log.Fatalf("Error executing SQL: %v", err)
+	}
+
+	fmt.Println("Database initialized successfully")
+}
