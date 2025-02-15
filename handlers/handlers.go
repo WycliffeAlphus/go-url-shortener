0c7ea342e8225c00f8abcadeec55f36389245e63
WycliffeAlphus <wycliffealphus7@gmail.com>
2025-02-15 21:09:44 +0300

    feat:add homehandler

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -2,19 +2,53 @@ package handlers
 
 import (
 	"net/http"
+	"text/template"
 
 	"urlShortener/models"
 )
 
 type PageData struct {
-	BaseURL, Error string
-	URLData        []*models.URLStore
+	BaseURL string
+	Error   string
+	URLData []*models.URLStore
 }
 
 type App struct {
 	urls *models.ShortenerDataModel
 }
 
+func NewApp(urls *models.ShortenerDataModel) *App {
+	return &App{
+		urls: urls,
+	}
+}
+
+func (app *App) HandleHome() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path != "/" {
+			http.NotFound(w, r)
+			return
+		}
+
+		urls, err := app.urls.Latest()
+
+		if err != nil {
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+
+		data := PageData{
+			BaseURL: "http://" + r.Host,
+			URLData: urls,
+		}
+		tmpl, err := template.ParseFiles("templates/home.html")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		tmpl.Execute(w, data)
+	}
+}
 
 func HandleRedirect(w http.ResponseWriter, r *http.Request) {
 	shortURL := r.URL.Path[1:]
