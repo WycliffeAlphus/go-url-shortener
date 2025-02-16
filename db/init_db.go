5d6d30af9ca5b42f3f1e595704949d0e0d1d0276
WycliffeAlphus <wycliffealphus7@gmail.com>
2025-02-16 15:33:01 +0300

    feat:change it to return db instance

--- db/init_db.go
+++ db/init_db.go
@@ -2,32 +2,34 @@ package db
 
 import (
 	"database/sql"
+	"errors"
 	"fmt"
-	"log"
 	"os"
 
 	_ "github.com/mattn/go-sqlite3"
 )
 
 // InitDB initializes the SQLite database by executing the schema.sql script.
-func InitDB(dbPath, sqlFile string) {
+func InitDB(dbPath, sqlFile string) (*sql.DB, error) {
 	db, err := sql.Open("sqlite3", dbPath)
 	if err != nil {
-		log.Fatalf("Error opening database: %v", err)
+
+		return nil, errors.New("Error opening database: " + err.Error())
 	}
 
 	defer db.Close()
 
 	sqlBytes, err := os.ReadFile(sqlFile)
 	if err != nil {
-		log.Fatalf("Error reading SQL file: %v", err)
+		return nil, errors.New("Error reading SQL file: " + err.Error())
 	}
 	sqlStatements := string(sqlBytes)
 
 	_, err = db.Exec(sqlStatements)
 	if err != nil {
-		log.Fatalf("Error executing SQL: %v", err)
+		return nil, errors.New("Error executing SQL: " + err.Error())
 	}
 
 	fmt.Println("Database initialized successfully")
+	return db, nil
 }
